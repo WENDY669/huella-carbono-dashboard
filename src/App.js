ce8bd478665e84801ddc1f8b1c9ad193658dee17
WENDY669 <220300772@ucaribe.edu.mx>
2025-10-26 22:52:59 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Dashboard Huella de Carbono</h1>
+            <p>Análisis de emisiones globales de CO2</p>
+        </div>
+    );
+}
+
+export default App;
